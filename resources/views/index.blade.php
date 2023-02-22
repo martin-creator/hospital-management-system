b5ac61aedbe5f5e55cc5a3eb100c0dec396a80c9
martin-creator <martinlubowa@outlook.com>
2023-02-22 12:37:26 +0300

    Added boostrap grids

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,18 @@
 @extends('layouts.main')
 
 @section('content')
-
-<div class="container-lg" style="margin:0 auto;">
-    <div class="row mt-5">
-        <div class="col-lg-4 col-md-4 col-sm-12 text-center mx-5 mb-3">
-            <div class="card" style="width: 28rem">
-                <img/>
-                <div class="card-body">
-                    <div class="card-title"> Title </div>
-                    <div class="card-text"> Content </div>
+    <div class="container-lg" style="margin:0 auto;">
+        <div class="row mt-5">
+            <div class="col-lg-4 col-md-4 col-sm-12 text-center mb-3">
+                <div class="card" style="width: 18rem">
+                    <img />
+                    <div class="card-body">
+                        <div class="card-title"> Title </div>
+                        <div class="card-text"> Content </div>
+                    </div>
                 </div>
             </div>
+
         </div>
     </div>
-</div>
-    
 @endsection
