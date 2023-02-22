bbb4a9b3a09bc2652075499e62f2698116f08832
martin-creator <martinlubowa@outlook.com>
2023-02-22 19:51:23 +0300

    Fixed format

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,18 +3,17 @@
 @section('content')
     <div class="container-lg" style="margin:0 auto;">
         <div class="row mt-5 ms-5">
-        @foreach ($departments as $department)
-        <div class="col-lg-4 col-md-4 col-sm-12 text-center mb-3">
-            <div class="card" style="width: 18rem">
-                <img  src="{{$department->image}}" style="width:200px; margin: 0 auto;"/>
-                <div class="card-body">
-                    <div class="card-title"> {{ $department->name}} </div>
-                    <div class="card-text"> {{$department->description}} </div>
+            @foreach ($departments as $department)
+                <div class="col-lg-4 col-md-4 col-sm-12 text-center mb-3">
+                    <div class="card" style="width: 18rem">
+                        <img src="{{ $department->image }}" style="width:200px; margin: 0 auto;" />
+                        <div class="card-body">
+                            <div class="card-title"> {{ $department->name }} </div>
+                            <div class="card-text"> {{ $department->description }} </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        @endforeach
-
+            @endforeach
         </div>
     </div>
 @endsection
